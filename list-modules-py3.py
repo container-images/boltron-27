1358cabdf8736f100c8bbe5ad479f1e1cf200fe8
James Antill <james@and.org>
2017-10-06 00:37:13 -0400

    Add profiles.

--- list-modules-py3.py
+++ list-modules-py3.py
@@ -11,8 +11,8 @@ mods = []
 for module in base.repo_module_dict.values():
     for stream in module.values():
         for version in stream.values():
-            mods.append((module.name, stream.stream, version.version))
+            mods.append((module.name, stream.stream, version.version, ",".join(sorted(version.profiles))))
 
 for i in sorted(mods):
-    print("%20s %20s %20s" % i)
+    print("%20s %10s %16s %s" % i)
 
